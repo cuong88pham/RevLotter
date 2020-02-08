37517b14e61fc9711f80fb821f9d6d52238c29d7
Giang Huynh <trgianghuynhdev1808@gmail.com>
2020-02-08 16:31:48 +0700

    implement products section component

--- packages/client/components/PageHomeComponent.js
+++ packages/client/components/PageHomeComponent.js
@@ -1,10 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import HeroSectionComponent from './HeroSectionComponent';
+import ProductsSectionComponent from './ProductsSectionComponent';
 
 export default function PageHome() {
   return (
     <div id="main" className="main">
       <HeroSectionComponent />
+      <ProductsSectionComponent />
     </div>
   );
 }
