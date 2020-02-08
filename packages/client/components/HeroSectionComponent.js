6df13313aa4dec3b4cf390183d6eb510d0fb4102
Giang Huynh <trgianghuynhdev1808@gmail.com>
2020-02-08 16:17:04 +0700

    update button layout, implement HeroSection

---
+++ packages/client/components/HeroSectionComponent.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import Button from '../components/common/Button';
+
+const HeroSectionComponent = () => {
+  return (
+    <div className="home">
+      <div className="container">
+        <div className="row text-center">
+          <div className="col-md-12">
+            <div className="hero-img wow fadeIn">
+              <img
+                className="img-fluid"
+                src="/static/images/big.png"
+                alt="Home"
+              />
+            </div>
+          </div>
+          <div className="col-md-12">
+            <div className="hero-content wow fadeIn">
+              <h1>The Lottery online based on blockchain</h1>
+              <p>
+                Transparent and fair jackpot. Be the first one to join and get
+                free Ticket.
+              </p>
+              <Button
+                href="#signup"
+                text={'Register now'}
+                isScrollButton={true}
+              />
+            </div>
+          </div>
+          <div className="col-md-12">
+            <div className="client-list wow fadeIn">
+              <ul>
+                <li>
+                  <img
+                    className="img-fluid"
+                    src="/static/icons/1.png"
+                    alt="Client"
+                  />{' '}
+                </li>
+                <li>
+                  <img
+                    className="img-fluid"
+                    src="/static/icons/2.png"
+                    alt="Client"
+                  />{' '}
+                </li>
+                <li>
+                  <img
+                    className="img-fluid"
+                    src="/static/icons/3.png"
+                    alt="Client"
+                  />{' '}
+                </li>
+                <li>
+                  <img
+                    className="img-fluid"
+                    src="/static/icons/4.png"
+                    alt="Client"
+                  />{' '}
+                </li>
+                <li>
+                  <img
+                    className="img-fluid"
+                    src="/static/icons/5.png"
+                    alt="Client"
+                  />{' '}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSectionComponent;
