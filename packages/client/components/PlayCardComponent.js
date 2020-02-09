abe153cb29e0ebd29bde767e5a79034da4a5e824
Giang Huynh <trgianghuynhdev1808@gmail.com>
2020-02-10 00:29:34 +0700

    change function component to class

--- packages/client/components/PlayCardComponent.js
+++ packages/client/components/PlayCardComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
@@ -36,7 +36,6 @@ const pickNumber = (
         activeNumbers.splice(indexItem, 1);
       }
     }
-
     setActiveNumbers(activeNumbers);
   } else {
     setActiveNumbers(value);
@@ -61,77 +60,92 @@ const NumberList = ({ maxSize, activeNumbers, setActiveNumbers }) => {
   return numberList;
 };
 
-const PlayCardComponent = () => {
-  const [activeNumbers, setActiveNumbers] = React.useState([]);
-  const [activeNumber, setActiveNumber] = React.useState();
+class PlayCardComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeNumbers: [],
+      activeNumber: null
+    };
+  }
+  setActiveNumbers = activeNumbers => {
+    this.setState({ ...this.state, activeNumbers });
+  };
 
-  return (
-    <div className="play-card">
-      <button type="button" className="close-play-card">
-        <i className="fa fa-times">x</i>
-      </button>
-      <div className="play-card-inner text-center">
-        <div className="play-card-header">
-          <span className="number-amount">Pick 5 Numbers</span>
-          <div className="header-btn-area">
-            <button
-              type="button"
-              id="quick-pick1"
-              onClick={() => {
-                const randomIntArray = getRandomIntArray(50, 5);
-                setActiveNumbers(randomIntArray);
-                setActiveNumber(getRandomInt(1, 10));
-              }}
-            >
-              quick pick
-            </button>
-            <button
-              type="button"
-              id="clear-pick1"
-              onClick={() => {
-                setActiveNumbers([]);
-                setActiveNumber();
-              }}
-            >
-              clear
-            </button>
+  setActiveNumber = activeNumber => {
+    this.setState({ ...this.state, activeNumber });
+  };
+  render() {
+    const { activeNumber, activeNumbers } = this.state;
+
+    return (
+      <div className="play-card">
+        <button type="button" className="close-play-card">
+          <i className="fa fa-times">x</i>
+        </button>
+        <div className="play-card-inner text-center">
+          <div className="play-card-header">
+            <span className="number-amount">Pick 5 Numbers</span>
+            <div className="header-btn-area">
+              <button
+                type="button"
+                id="quick-pick1"
+                onClick={() => {
+                  const randomIntArray = getRandomIntArray(50, 5);
+                  this.setActiveNumbers(randomIntArray);
+                  this.setActiveNumber(getRandomInt(1, 10));
+                }}
+              >
+                quick pick
+              </button>
+              <button
+                type="button"
+                id="clear-pick1"
+                onClick={() => {
+                  this.setActiveNumbers([]);
+                  this.setActiveNumber();
+                }}
+              >
+                clear
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="play-card-body">
-          <ul className="number-list">
-            <NumberList
-              maxSize={50}
-              activeNumbers={activeNumbers}
-              setActiveNumbers={setActiveNumbers}
-            />
-          </ul>
-          <span className="add-more-text">Pick 1 numbers</span>
-          <ul className="number-list">
-            <NumberList
-              maxSize={10}
-              activeNumbers={[activeNumber]}
-              setActiveNumbers={setActiveNumber}
-            />
-          </ul>
-        </div>
-        <div className="play-card-footer">
-          <p className="play-card-footer-text">Selected Numbers:</p>
-          <div className="selected-numbers">
-            {!activeNumber && activeNumbers.length === 0 ? (
-              <span className="pt-1">No selected number</span>
-            ) : (
-              activeNumbers.map((activeNumber, index) => (
-                <span className="p-2" key={index}>
-                  {activeNumber}
-                </span>
-              ))
-            )}
-            <span style={{ fontWeight: 'bold' }}>{activeNumber}</span>
+          <div className="play-card-body">
+            <ul className="number-list">
+              <NumberList
+                maxSize={50}
+                activeNumbers={activeNumbers}
+                setActiveNumbers={this.setActiveNumbers}
+              />
+            </ul>
+            <span className="add-more-text">Pick 1 numbers</span>
+            <ul className="number-list">
+              <NumberList
+                maxSize={10}
+                activeNumbers={[activeNumber]}
+                setActiveNumbers={this.setActiveNumber}
+              />
+            </ul>
+          </div>
+          <div className="play-card-footer">
+            <p className="play-card-footer-text">Selected Numbers:</p>
+            <div className="selected-numbers">
+              {!activeNumber && activeNumbers.length === 0 ? (
+                <span className="pt-1">No selected number</span>
+              ) : (
+                activeNumbers.map((activeNumber, index) => (
+                  <span className="p-2" key={index}>
+                    {activeNumber}
+                  </span>
+                ))
+              )}
+              <span style={{ fontWeight: 'bold' }}>{activeNumber}</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default PlayCardComponent;
