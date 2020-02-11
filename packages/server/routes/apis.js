535d7ff693e8ded930fbbf1eb77b909474fe45a7
CuongPham <harry88pham@gmail.com>
2020-02-12 01:03:55 +0700

    add auth api

--- packages/server/routes/apis.js
+++ packages/server/routes/apis.js
@@ -1,34 +1,42 @@
 import { Router } from 'express';
-import MetaAuth from 'meta-auth';
-
-import { userSchema, setSchema } from '../services';
-
-const metaAuth = new MetaAuth({
-  banner: 'Revlotter Authen'
-});
-
+import { JWT } from '../services';
+const ethUtil = require('ethereumjs-util');
 const router = Router();
+router.get('/', (req, res) => {
+  res.send({ status: 'OKIE' });
+});
 
-// get challenge
-router.get('/auth/:MetaAddress', metaAuth, (req, res) => {
-  // Request a message from the server
-  if (req.metaAuth && req.metaAuth.challenge) {
-    res.send(req.metaAuth.challenge);
+// get info by address
+router.get('/:address', (req, res) => {
+  res.send({ address: req.params.address });
+});
+
+// Login with metamask
+router.post('/auth', (req, res) => {
+  try {
+    const { signature, address } = req.body;
+    const sigParams = ethUtil.fromRpcSig(signature);
+    const msg = new Buffer.from(process.env.METAMASK_MSG);
+    const msgBuffer = ethUtil.toBuffer(msg);
+    const msgHash = ethUtil.hashPersonalMessage(msgBuffer);
+    const publicKey = ethUtil.ecrecover(
+      msgHash,
+      sigParams.v,
+      sigParams.r,
+      sigParams.s
+    );
+    const sender = ethUtil.publicToAddress(publicKey);
+    const senderAddress = ethUtil.bufferToHex(sender);
+    if (senderAddress === address) {
+      res.send({
+        token: 'bearer ' + JWT.signToken({ address: senderAddress })
+      });
+    } else {
+      res.status(401).send({ error: 'Unauthorized' });
+    }
+  } catch (error) {
+    res.status(500).send({ error });
   }
 });
 
-// verifySignature
-router.get('/auth/:MetaMessage/:MetaSignature', metaAuth, (req, res) => {
-  if (req.metaAuth && req.metaAuth.recovered) {
-    // Signature matches the cache address/challenge
-    // Authentication is valid, assign JWT, etc.
-    res.send(req.metaAuth.recovered);
-  } else {
-    // Sig did not match, invalid authentication
-    res.status(400).send();
-  }
-});
-const obj = { address: 'xxx' };
-const user = setSchema(userSchema, obj);
-console.log(user);
 export default router;
