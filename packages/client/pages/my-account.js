5386422f89298b5d74beddf143e66b2a33616768
DuongTran <ttduongtran@gmail.com>
2020-02-14 16:31:26 +0700

    implement page account

---
+++ packages/client/pages/my-account.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import React from 'react';
+import PageLayout from '../layouts/PageLayout';
+import PageLotteryProfileComponent from '../components/PageLotteryProfileComponent';
+
+const ProfilePage = () => (
+  <PageLayout>
+    <Head>
+      <title key="title">My Account | RevLotter</title>
+    </Head>
+    <PageLotteryProfileComponent />
+  </PageLayout>
+);
+
+export default ProfilePage;
