b14f09a4537a90d4c271a54cab02154c14d490cd
DuongTran <ttduongtran@gmail.com>
2020-02-18 16:50:30 +0700

    add row number of winners

--- packages/client/components/ResultsTablePrizesComponent.js
+++ packages/client/components/ResultsTablePrizesComponent.js
@@ -8,6 +8,7 @@ function ResultsTablePrizesComponent({ prizeBreakdown = [], t }) {
         <tr>
           <th scope="col">{t('results.division')}</th>
           <th scope="col">{t('results.match')}</th>
+          <th scope="col">{t('results.number_of_winners')}</th>
           <th scope="col">{t('results.payout_per_winner')}</th>
           <th scope="col">{t('results.jackpot')}</th>
           <th scope="col">{t('results.payout_per_winners')}</th>
@@ -19,6 +20,7 @@ function ResultsTablePrizesComponent({ prizeBreakdown = [], t }) {
             <tr key={index}>
               <td>{row.division}</td>
               <td>{row.match}</td>
+              <td>{row.numberOfWinners}</td>
               <td>{row.payoutPerWinner}</td>
               <td>{row.jackpot}</td>
               <td>{row.payoutPerWinners}</td>
