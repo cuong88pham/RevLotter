a0194a1de1bc5a3c6b91d60eb07178bf78f23053
DuongTran <ttduongtran@gmail.com>
2020-02-19 17:59:22 +0700

    add content page about us

---
+++ packages/client/components/AboutUsComponent.js
@@ -0,0 +1,59 @@
+import React, { Fragment } from 'react';
+
+export default function AboutUsComponent() {
+  return (
+    <Fragment>
+      <div className="text-center">
+        <img src="/static/images/big.png" alt="" />
+      </div>
+      <h1 className="title">ABOUT US</h1>
+      <div className="">
+        <p className="ml-0 mr-0 w-100 mw-100 text-dark">
+          CSC (Crypto Sharing Community) was founded with the belief that
+          everyone should own some Bitcoin and benefit from the emerging
+          Blockchain Industry. To do this we have launched a Blockchain based
+          Lotto Game with a growing Jackpot paid in Bitcoin. You can share the
+          game with your friends to increase your chances of winning. The more
+          people that play, the bigger the Jackpot becomes!
+        </p>
+        <p className="ml-0 mr-0 w-100 mw-100 text-dark">
+          In the near future we will launch new types of games, and expand our
+          portfolio with new products and services that will bring excitement
+          and enrich your life!
+        </p>
+        <p className="ml-0 mr-0 w-100 mw-100 text-dark">
+          The best part is that entrepreneurial players can earn additional
+          commissions and bonuses as CSC Ambassadors. How would you like earning
+          Bitcoin every time your referrals play, or win? Wouldn’t that be
+          awesome!?
+        </p>
+        <p className="ml-0 mr-0 w-100 mw-100 text-dark">
+          Register for free and get a chance to reach your dreams with CSC.
+        </p>
+      </div>
+      <h1 className="title">PRIVACY POLICY</h1>
+      <div className="">
+        <p className="ml-0 mr-0 w-100 mw-100 text-dark">
+          Our Privacy Policy is meant to help you understand what information we
+          collect, why we collect it, and how you can update, manage, export,
+          and delete your information.
+        </p>
+        <p className="ml-0 mr-0 w-100 mw-100 text-dark">
+          We care about Your privacy. AND You need to read our privacy policy.
+        </p>
+        <p className="ml-0 mr-0 w-100 mw-100 text-dark">
+          If you do not agree with this Policy, do not use our products and
+          services or provide HERE with your personal data.
+        </p>
+      </div>
+      <h1 className="title">TERM & CONDITIONS</h1>
+      <div className="">
+        <p className="ml-0 mr-0 w-100 mw-100 text-dark">
+          Please read the following terms and conditions carefully as it sets
+          out the terms of a legally binding agreement between you (the
+          ‘‘users’’) and Our company.
+        </p>
+      </div>
+    </Fragment>
+  );
+}
