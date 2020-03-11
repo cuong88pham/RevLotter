4c5419c445002f65cc2d1f4b88a2c24d5a75f81b
StorkyLe <quangnhat2429@gmail.com>
2020-03-11 17:52:00 +0700

    ref(server) edit structure ticket, add status, expire time and  add check time to refuse ticket

--- packages/server/graphql/schemas/ticket/ticket.resolver.js
+++ packages/server/graphql/schemas/ticket/ticket.resolver.js
@@ -1,10 +1,11 @@
 import { UserModel } from '../../../models';
+import { WAIT_RESULT, WIN, LOSE } from '../../enums/ticketStatus';
 export default {
   Ticket: {
     id: payload => payload.id,
     owner: async payload => {
-      console.log({ payload });
       return await UserModel.detailUserById(payload.owner_id);
     }
-  }
+  },
+  TicketStatus: { WAIT_RESULT, WIN, LOSE }
 };
