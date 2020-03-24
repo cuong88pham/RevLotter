671b015f744752b26f5d2d15e57832b14c4da612
StorkyLe <quangnhat2429@gmail.com>
2020-03-24 15:01:48 +0700

    ref(server) add type application/graphql to bodyparser

--- packages/server/server.js
+++ packages/server/server.js
@@ -14,6 +14,7 @@ app.use(helmet());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ type: 'application/json', extended: true }));
+app.use(bodyParser.text({ type: 'application/graphql' }));
 app.use(cookieParser());
 app.use(logger('dev'));
 app.use(apis);
