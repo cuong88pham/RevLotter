20b95b6acedbaccf83912923c38f2c337cec8b32
Giang Huynh <trgianghuynhdev1808@gmail.com>
2020-03-24 15:50:37 +0700

    make clear code

--- packages/client/stores/middlewares/index.js
+++ packages/client/stores/middlewares/index.js
@@ -1,18 +1,16 @@
 import reduxThunk from 'redux-thunk';
 import { createAPIMiddleware, composeAdapters } from 'redux-api-call';
-import { intercepter as graphql } from '../../libs';
-
 import fetchInterceptor from 'redux-api-call-adapter-fetch';
 import jsonInterceptor from 'redux-api-call-adapter-json';
 
+import { intercepter as graphql } from '../../libs';
+import { isServer } from '../../utils';
 import { middleware as apiReactionMiddleware } from './api-reaction';
 import apiPrefix from './api-prefix';
 
 const env = process.env.NODE_ENV || 'development';
 const base =
   process.env.API_SERVER_URL || 'https://api.thecryptolotter.io/staging';
-
-export const isServer = !process.browser;
 
 const logger = () => next => action => {
   isServer && env === 'development' && console.log('REDUX: %s', action.type);
